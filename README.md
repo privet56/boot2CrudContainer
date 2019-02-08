9cf2eb42ede31ce813a0b20a256e149c46969e6b
privet <privet56@hotmail.com>
2019-02-08 18:01:11 +0100

    Docker build and docu

--- README.md
+++ README.md
@@ -3,10 +3,11 @@ Spring Boot App with REST WebServices in Docker Container with Kubernetes and He
 
 ## Setup on Ubunut
 ### installed through ui: krusader, kate, konsole, docker, minikube
-### installed through cmd: jdk, curl, git
+### installed through cmd: jdk, curl, git, dockerd(docker.io)
 	$ sudo apt install default-jdk
 	$ sudo apt install curl
 	$ sudo apt install git
+	$ sudo apt install docker.io
 ### installed through web: sts
 
 ## Java Development: with STS - Spring Tool Suite 4
@@ -16,6 +17,14 @@ Spring Boot App with REST WebServices in Docker Container with Kubernetes and He
 ### Editor (JSON or YAML)
 <img src="_res/swagger.editor.png" width="650px">
 <img src="_res/swagger.editor.2.png" width="650px">
+
 ### Browser - showing the REST API of the App
 http://localhost:8080/swagger-ui.html
 <img src="_res/swagger.api.png" width="650px">
+
+## Docker build
+(optional: you can docker-build a a maven-goal too (with a maven plug-in inserted in your pom.xml))
+```sh
+sudo docker build .
+sudo docker run -p 8080:8080 76f0e2cf29a3
+```
--- src/main/java/com/ng/crud/controller/EventController.java
+++ src/main/java/com/ng/crud/controller/EventController.java
@@ -3,6 +3,11 @@ package com.ng.crud.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +24,7 @@ import com.ng.crud.model.Event;
 @RequestMapping(value = "/api/event")
 public class EventController
 {
+	private static final Logger logger = LoggerFactory.getLogger(EventController.class);
 //GET - ALL
 	@RequestMapping(method = RequestMethod.GET)
     public @ResponseBody List<Event> events()
@@ -29,25 +35,56 @@ public class EventController
 //GET - ONE
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
     //public @ResponseBody Event event(@RequestParam("id") Long id)
-    public @ResponseBody Event event(@PathVariable("id") Long id)
+    public @ResponseBody Event event(@PathVariable("id") Long id, HttpServletResponse response)
 	{
+		if((id == null) || (id.equals(0L)))
+		{
+			logger.warn("event-get(1) - no/invalid id:"+id);
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			return null;
+		}
+		
         return new Event();
     }
 //CREATE
 	@PostMapping()
-	Long newEvent(@RequestBody Event newEvent) {
+	Long newEvent(@RequestBody Event newEvent, HttpServletResponse response)
+	{
+		if((newEvent.getId() != null) && !newEvent.getId().equals(0L))
+		{
+			logger.warn("event-post - new event should have no id:"+newEvent.getId());
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			return null;
+		}
+
 		//return repository.save(newEvent);
 		return newEvent.getId();
 	}
 //EDIT
 	@PutMapping(value = "/{id}")
-	boolean editEvent(@RequestBody Event event) {
+	boolean editEvent(@RequestBody Event event, HttpServletResponse response)
+	{
+		if((event.getId() == null) || event.getId().equals(0L))
+		{
+			logger.warn("event-put - event to be edited, should have id:"+event.getId());
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			return false;
+		}
+
 		//return repository.save(newEvent);
 		return false;
 	}
 //DEL
 	@DeleteMapping("/{id}")
-	boolean delEvent(@PathVariable("id") Long id) {
+	boolean delEvent(@PathVariable("id") Long id, HttpServletResponse response)
+	{
+		if((id == null) || id.equals(0L))
+		{
+			logger.warn("event-del - event to be deleted, should have id:"+id);
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			return false;
+		}
+
 		//return repository.save(newEvent);
 		return false;
 	}	
